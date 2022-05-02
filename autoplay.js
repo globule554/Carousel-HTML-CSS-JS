c83a455c30728b420eeda0ab6afa3b447462f8bf
Malek MOKRANI <malek.mokrani@outlook.com>
2022-05-02 09:06:34 +0200

    next & prev handlers

--- autoplay.js
+++ autoplay.js
@@ -2,19 +2,34 @@ window.addEventListener('load', autoplay);
 
 function autoplay(event)
 {
-    let elts = document.querySelectorAll(".rb");
+    const elts = document.querySelectorAll(".rb");
+    const max = elts.length-1;
     let index = 0;
-    let max = elts.length-1;
-
+    
     const nextFrame = ()=>{
         return  setInterval(()=>{
-             index = index < max ? index +1 : 0
-             elts[index].checked = true;
-         }, 3000)
-     }
+            index = index < max ? index +1 : 0
+            elts[index].checked = true;
+        }, 3000)
+    }
 
+    const prevNext = (event)=>{
+       
+            clearInterval(timer);
+            if(event.target==nxt)   index = index < max ? index +1 : 0;
+            else index = index > 0 ? index - 1 : max;
+            elts[index].checked = true;
+            timer = nextFrame();
+       
+    }
+    
     let timer = nextFrame();
- 
+
+    const nxt = document.querySelector(".next");
+    const prv = document.querySelector(".prev");
+    
+    nxt.addEventListener('click', prevNext);
+    prv.addEventListener('click',  prevNext);
 
     elts.forEach(rb=>{
         rb.addEventListener("change" ,(event)=>{
